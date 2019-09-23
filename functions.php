669642bc428d677c1e6d672c1454d59b1d053e81
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-09-23 15:28:19 +0100

    added blank CSS file, blank unit test file, moved functions across to a dedicated functions file

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Simple function to retrieve data from database
+ *
+ * @return array containing all data from database
+ */
+function retrieveData(): array {
+    $db = new PDO('mysql:host=db;dbname=Disease_db', 'root', 'password');
+
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $query = $db->query('SELECT `Organism`, `Incubation_usual`, `Incubation_range`, `Symptoms`, `Severity`, `Avg_annual_incidence` FROM disease_table');
+
+    $output = $query->fetchAll();
+
+    return $output;
+}
+
+/*
+ * Produce html text to display data
+ *
+ * @param array an array to present in the browser
+ *
+ * @return string a string containing html code
+ */
+function displayData(array $data): string {
+    $htmlToOutput = null;
+
+    foreach ($data as $row) {
+        $htmlToOutput = '<div class="disease.item">' . $htmlToOutput;
+        foreach ($row as $key => $value) {
+            $htmlToOutput = $htmlToOutput . '<strong>' . $key . '</strong>: ' . $value . '<br>';
+        }
+        $htmlToOutput = $htmlToOutput . '</div>' .'<br>';
+    }
+
+    return $htmlToOutput;
+}
--- index.php
+++ index.php
@@ -1,42 +1,5 @@
 <?php
-
-/*
- * Simple function to retrieve data from database
- *
- * @return array containing all data from database
- */
-function retrieveData(): array {
-    $db = new PDO('mysql:host=db;dbname=Disease_db', 'root', 'password');
-
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-    $query = $db->query('SELECT `Organism`, `Incubation_usual`, `Incubation_range`, `Symptoms`, `Severity`, `Avg_annual_incidence` FROM disease_table');
-
-    $output = $query->fetchAll();
-
-    return $output;
-}
-
-/*
- * Produce html text to display data
- *
- * @param array an array to present in the browser
- *
- * @return string a string containing html code
- */
-function displayData(array $data): string {
-    $htmlToOutput = null;
-
-    foreach ($data as $row) {
-        $htmlToOutput = '<div>' . $htmlToOutput;
-        foreach ($row as $key => $value) {
-            $htmlToOutput = $htmlToOutput . '<strong>' . $key . '</strong>: ' . $value . '<br>';
-        }
-        $htmlToOutput = $htmlToOutput . '</div>' .'<br>';
-    }
-
-    return $htmlToOutput;
-}
+require('functions.php');
 
 $allData = retrieveData();
 
@@ -44,3 +7,14 @@ echo displayData($allData);
 
 ?>
 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>title</title>
+</head>
+<body>
+
+</body>
+</html>
