1e5a2e71eb47fc063f1f8a9568bd9a7a2ae871b9
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-09-23 13:03:18 +0100

    made start on query

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$db = new PDO('mysql:host=db;dbname=Disease_db', 'root', 'password');
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$db->query('SELECT `Organism`,  ')
+$query = $
