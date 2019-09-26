b7608e0c69fff182936f4e02921fd3b6f122b190
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-09-26 11:06:57 +0100

    made editable form popup in edit_item

--- edit_item.php
+++ edit_item.php
@@ -5,47 +5,98 @@ $db = establishDisease_dbConnection();
 $allData = retrieveData($db);
 $allOrganisms = grabAllOrganisms($allData);
 
-if (isset($_POST['submit'])) {
+//if (isset($_POST['submit'])) {
+//    $organismArray = grabDataForSelectOrganism($allData);
+//
+//    var_dump(grabDataForSelectOrganism($allData));
+//    $formWithGrabbedData = '<form method="post" enctype="multipart/form-data">
+//    <p>Organism</p>
+//    <p><input type="text" name="organism" value = "' .
+//        $organismArray['Organism'] .
+//        '"></p><p>Incubation usual</p><p><input type="text" name="incubation_usual" value="' .
+//        $organismArray['Incubation_usual'] .
+//        '"></p><p>Incubation range</p><p><input type="text" name="incubation_range" value="' .
+//        $organismArray['Incubation_range'] .
+//        '"></p><p>Symptoms</p><p><input type="text" name="symptoms" value="' .
+//        $organismArray['Symptoms'] .
+//        '"></p><p>Severity</p><p><input type="text" name="severity" value="' .
+//        $organismArray['Severity'] .
+//        '"></p><p>Average annual incidence</p><p><input type="number" name="avg_annual_incidence" value="' .
+//        $organismArray['Avg_annual_incidence'] .
+//        '"></p>
+//    <p>Image</p><p><input type="file" name="img"></p><p><input type="submit" value="Edit disease"></p></form>' ;
+//
+//
+//
+//}
 
-}
+//
+//if (isset($_POST['organism']) &&
+//    isset($_POST['incubation_usual']) &&
+//    isset($_POST['incubation_range']) &&
+//    isset($_POST['symptoms']) &&
+//    isset($_POST['severity']) &&
+//    isset($_POST['avg_annual_incidence']) &&
+//    isset($_FILES['img']) &&
+//    $_FILES['img']['name'] != '') {
+//
+//}
+//
 
 
-if (isset($_POST['organism']) &&
-    isset($_POST['incubation_usual']) &&
-    isset($_POST['incubation_range']) &&
-    isset($_POST['symptoms']) &&
-    isset($_POST['severity']) &&
-    isset($_POST['avg_annual_incidence']) &&
-    isset($_FILES['img']) &&
-    $_FILES['img']['name'] != '') {
+//if (isset($_POST['edit-organism'])) {
+//    var_dump(grabDataForSelectOrganism($allData));
+//    echo '<form method="post" enctype="multipart/form-data">
+//    <p>Organism</p>
+//    <p><input type="text" name="organism" value = "' .
+//    $organismArray['Organism'] .
+//    '"></p><p>Incubation usual</p><p><input type="text" name="incubation_usual" value="' .
+//    $organismArray['Incubation_usual'] .
+//    '"></p><p>Incubation range</p><p><input type="text" name="incubation_range" value="' .
+//    $organismArray['Incubation_range'] .
+//    '"></p><p>Symptoms</p><p><input type="text" name="symptoms" value="' .
+//    $organismArray['Symptoms'] .
+//    '"></p><p>Severity</p><p><input type="text" name="severity" value="' .
+//    $organismArray['Severity'] .
+//'"></p><p>Average annual incidence</p><p><input type="number" name="avg_annual_incidence" value="' .
+//$organismArray['Avg_annual_incidence'] .
+//'"></p>
+//    <p>Image</p><p><input type="file" name="img"></p><p><input type="submit" value="Edit disease"></p></form>' ;
+//} else {
+//
+//}
 
-}
 
-$organismArray = grabDataForSelectOrganism($allData);
 
 if (isset($_POST['edit-organism'])) {
-    var_dump(grabDataForSelectOrganism($allData));
-    echo '<form method="post" enctype="multipart/form-data">
-    <p>Organism</p>
-    <p><input type="text" name="organism" value = "' .
-    $organismArray['Organism'] .
-    '"></p><p>Incubation usual</p><p><input type="text" name="incubation_usual" value="' .
-    $organismArray['Incubation_usual'] .
-    '"></p><p>Incubation range</p><p><input type="text" name="incubation_range" value="' .
-    $organismArray['Incubation_range'] .
-    '"></p><p>Symptoms</p><p><input type="text" name="symptoms" value="' .
-    $organismArray['Symptoms'] .
-    '"></p><p>Severity</p><p><input type="text" name="severity" value="' .
-    $organismArray['Severity'] .
-'"></p><p>Average annual incidence</p><p><input type="number" name="avg_annual_incidence" value="' .
-$organismArray['Avg_annual_incidence'] .
-'"></p>
-    <p>Image</p><p><input type="file" name="img"></p><p><input type="submit" value="Edit disease"></p></form>' ;
+
+    $organismArray = grabDataForSelectOrganism($allData);
+
+    function createOrganismEditForm(array $organismArray): string {
+
+        return '<form method="post" enctype="multipart/form-data"><p>Organism</p><p><input type="text" name="organism" value = "' .
+            $organismArray['Organism'] .
+            '"></p><p>Incubation usual</p><p><input type="text" name="incubation_usual" value="' .
+            $organismArray['Incubation_usual'] .
+            '"></p><p>Incubation range</p><p><input type="text" name="incubation_range" value="' .
+            $organismArray['Incubation_range'] .
+            '"></p><p>Symptoms</p><p><input type="text" name="symptoms" value="' .
+            $organismArray['Symptoms'] .
+            '"></p><p>Severity</p><p><input type="text" name="severity" value="' .
+            $organismArray['Severity'] .
+            '"></p><p>Average annual incidence</p><p><input type="number" name="avg_annual_incidence" value="' .
+            $organismArray['Avg_annual_incidence'] .
+            '"></p><p>Image</p><p><input type="file" name="img"></p><p><input type="submit" value="Edit disease"></p></form>';
+
+    }
+
+    $formWithGrabbedOrganismData = createOrganismEditForm($organismArray);
+
+
 } else {
 
+    $formWithGrabbedOrganismData = '';
 }
-
-
 
 
 
@@ -60,6 +111,8 @@ $organismArray['Avg_annual_incidence'] .
     <link rel="stylesheet" href="styles.css" type="text/css">
 </head>
 <body>
+
+
 <div class="nav-panel">
     <nav>
         <div class="heading"><a href="index.php"><p>Collection</p></a></div>
@@ -79,30 +132,34 @@ $organismArray['Avg_annual_incidence'] .
     </form>
 </div>
 
-<div>
-    <p>Update details:</p>
+<div class="edit-item">
+<?php echo $formWithGrabbedOrganismData; ?>
 </div>
-
-
-<div class="container edit_item">
-    <form method="post" enctype="multipart/form-data">
-        <p>Organism</p>
-        <p><input type="text" name="organism" value = "organism1"></p>
-        <p>Incubation usual</p>
-        <p><input type="text" name="incubation_usual"></p>
-        <p>Incubation range</p>
-        <p><input type="text" name="incubation_range"></p>
-        <p>Symptoms</p>
-        <p><input type="text" name="symptoms"></p>
-        <p>Severity</p>
-        <p><input type="text" name="severity"></p>
-        <p>Average annual incidence</p>
-        <p><input type="number" name="avg_annual_incidence"></p>
-        <p>Image</p>
-        <p><input type="file" name="img"></p>
-        <p><input type="submit" value="Edit disease"></p>
-    </form>
-</div>
+<!---->
+<!--<div>-->
+<!--    <p>Update details:</p>-->
+<!--</div>-->
+<!---->
+<!---->
+<!--<div class="container edit_item">-->
+<!--    <form method="post" enctype="multipart/form-data">-->
+<!--        <p>Organism</p>-->
+<!--        <p><input type="text" name="organism" value = "organism1"></p>-->
+<!--        <p>Incubation usual</p>-->
+<!--        <p><input type="text" name="incubation_usual"></p>-->
+<!--        <p>Incubation range</p>-->
+<!--        <p><input type="text" name="incubation_range"></p>-->
+<!--        <p>Symptoms</p>-->
+<!--        <p><input type="text" name="symptoms"></p>-->
+<!--        <p>Severity</p>-->
+<!--        <p><input type="text" name="severity"></p>-->
+<!--        <p>Average annual incidence</p>-->
+<!--        <p><input type="number" name="avg_annual_incidence"></p>-->
+<!--        <p>Image</p>-->
+<!--        <p><input type="file" name="img"></p>-->
+<!--        <p><input type="submit" value="Edit disease"></p>-->
+<!--    </form>-->
+<!--</div>-->
 
 
 </body>
